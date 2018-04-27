3c221bf81f627aa5b600fab25c014119fd2f73fd
Paul Jacobs <paul.r.jacobs@gmail.com>
2018-04-27 12:10:59 -0700

    Update strncat_ovf2.c
    Copied in too much.

--- strncat_ovf2.c
+++ strncat_ovf2.c
@@ -6,24 +6,3 @@ main(int argc, char **argv)
 
   strncat(buffer, argv[2], 90);
 }
-Example 15 – strncpy1.c
-
-/* 
-   In this example, the attacker controls the third argument of strncpy,
-   making it unsafe.
-*/
-
-
-#include <stdio.h>
-
-main(int argc, char **argv)
-{
-  int incorrectSize = atoi(argv[1]);
-  int correctSize = atoi(argv[2]);
-  char *buffer = (char *)malloc(correctSize+1);
-
-
-  /* number of characters copied is based on user-supplied value */
-
-  strncpy(buffer, argv[3], incorrectSize);
-}
